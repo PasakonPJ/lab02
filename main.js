e86976c8b18c52d6ad3dadbfe2896b5a8c234f28
pasakon <kongpazam@gmail.com>
2021-07-08 22:26:00 +0700

    5.4
    change cart to [ ] push id

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true,
             details:['50% cotton', '30% wool', '20% polyester']
         }
     },methods:{
-        updateCart(){
-            this.cart +=1;
+        updateCart(id){
+            this.cart.push(id);
         }
     }
   
